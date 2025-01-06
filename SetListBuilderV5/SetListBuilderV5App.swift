4e55af38fa86c18d68b620ff7bbdf5ca4dae8f7c
“Jason <jsaito203@gmail.com>
2025-01-06 16:23:14 -0500

    Initial Commit

---
+++ SetListBuilderV5/SetListBuilderV5App.swift
@@ -0,0 +1,17 @@
+//
+//  SetListBuilderV5App.swift
+//  SetListBuilderV5
+//
+//  Created by Jason Saito on 1/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SetListBuilderV5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SetListBuilderV5Tests/SetListBuilderV5Tests.swift
@@ -0,0 +1,16 @@
+//
+//  SetListBuilderV5Tests.swift
+//  SetListBuilderV5Tests
+//
+//  Created by Jason Saito on 1/6/25.
+//
+
+import Testing
+
+struct SetListBuilderV5Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
